60c307fedcbfbaea012f809e5da19cda66fd6205
hector <hector.absantos@outlook.com>
2024-06-25 17:26:14 -0300

    Test - added 'should handle an array with multiple items correctly' to the tests

--- test/jest/gilded-rose.test.ts
+++ test/jest/gilded-rose.test.ts
@@ -110,4 +110,41 @@ describe('Gilded Rose', () => {
         const items = gildedRose.updateQuality();
         expect(items[0].quality).toBe(0);
     });
+
+    it('should handle an array with multiple items correctly', () => {
+      const items = [
+          new Item('Aged Brie', 2, 0),
+          new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
+          new Item('Sulfuras, Hand of Ragnaros', 0, 80),
+          new Item('Conjured Mana Cake', 3, 6),
+          new Item('foo', 10, 20),
+          new Item('Aged Brie', 10, 30),
+          new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
+          new Item('Sulfuras, Hand of Ragnaros', -1, 80),
+          new Item('Conjured Mana Cake', 1, 7),
+          new Item('normal', 0, 25)
+      ];
+      const gildedRose = new GildedRose(items);
+
+      const updatedItems = gildedRose.updateQuality();
+
+      const expectedResults = [
+          { name: 'Aged Brie', sellIn: 1, quality: 1 },
+          { name: 'Backstage passes to a TAFKAL80ETC concert', sellIn: 14, quality: 21 },
+          { name: 'Sulfuras, Hand of Ragnaros', sellIn: 0, quality: 80 },
+          { name: 'Conjured Mana Cake', sellIn: 2, quality: 4 },
+          { name: 'foo', sellIn: 9, quality: 19 },
+          { name: 'Aged Brie', sellIn: 9, quality: 31 },
+          { name: 'Backstage passes to a TAFKAL80ETC concert', sellIn: 4, quality: 50 },
+          { name: 'Sulfuras, Hand of Ragnaros', sellIn: -1, quality: 80 },
+          { name: 'Conjured Mana Cake', sellIn: 0, quality: 5 },
+          { name: 'normal', sellIn: -1, quality: 23 }
+      ];
+
+      for (let i = 0; i < updatedItems.length; i++) {
+          expect(updatedItems[i].name).toBe(expectedResults[i].name);
+          expect(updatedItems[i].sellIn).toBe(expectedResults[i].sellIn);
+          expect(updatedItems[i].quality).toBe(expectedResults[i].quality);
+      }
+  });
 });
